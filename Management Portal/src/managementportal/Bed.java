c52e04afc26b102f5097d51625e1ff7f0d58bf33
Lupe <Lupe@alondra>
2019-03-31 11:23:50 -0400

    I just added the Bed class

---
+++ Management Portal/src/managementportal/Bed.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managementportal;
+
+/**
+ *
+ * @author Lupe
+ */
+public class Bed {
+    private int bedID;              //NOT null
+    //private int visitID;          //NOT null
+    //private int lockerID;         //NOT null
+    private String typeOfBed;       //NOT null
+    private String bed_description; //null
+    
+    public Bed()
+    {
+        typeOfBed = "";
+        bed_description = "";
+    }
+    
+    public Bed(int bID)
+    {
+        bedID = bID;
+        typeOfBed = "";
+        bed_description = "";
+    }
+
+    public int getBedID() {
+        return bedID;
+    }
+
+    public void setBedID(int bedID) {
+        this.bedID = bedID;
+    }
+
+    public String getTypeOfBed() {
+        return typeOfBed;
+    }
+
+    public void setTypeOfBed(String typeOfBed) {
+        this.typeOfBed = typeOfBed;
+    }
+
+    public String getBed_description() {
+        return bed_description;
+    }
+
+    public void setBed_description(String bed_description) {
+        this.bed_description = bed_description;
+    }
+    
+    
+}
