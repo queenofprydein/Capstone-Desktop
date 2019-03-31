294cc17fda3178fce3189c1c21ea4540535294ff
Lupe <Lupe@alondra>
2019-03-31 13:04:35 -0400

    I just added the Schedule class

---
+++ Management Portal/src/managementportal/Schedule.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managementportal;
+
+/**
+ *
+ * @author Lupe
+ */
+public class Schedule {
+    private int shiftID;
+    private int volunteerID;
+    
+    public Schedule(int sID, int vID)
+    {
+        shiftID = sID;
+        volunteerID = vID;
+    }
+
+    public int getShiftID() {
+        return shiftID;
+    }
+
+    public void setShiftID(int shiftID) {
+        this.shiftID = shiftID;
+    }
+
+    public int getVolunteerID() {
+        return volunteerID;
+    }
+
+    public void setVolunteerID(int volunteerID) {
+        this.volunteerID = volunteerID;
+    }
+    
+    
+}
